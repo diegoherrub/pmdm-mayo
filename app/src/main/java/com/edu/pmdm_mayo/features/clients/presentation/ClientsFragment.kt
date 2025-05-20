aa0974e0b214ddb867221786023a5edfbfb33412
Heennaarr <henar.herher@educa.jcyl.es>
2025-05-20 17:46:13 +0200

    wip - working on clients

--- app/src/main/java/com/edu/pmdm_mayo/features/clients/presentation/ClientsFragment.kt
+++ app/src/main/java/com/edu/pmdm_mayo/features/clients/presentation/ClientsFragment.kt
@@ -5,8 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.edu.pmdm_mayo.R
 import com.edu.pmdm_mayo.databinding.ClientsFragmentBinding
-import com.edu.pmdm_mayo.features.clients.domain.models.Client
 import com.edu.pmdm_mayo.features.clients.presentation.adapter.ClientsAdapter
 
 class ClientsFragment : Fragment() {
@@ -33,17 +34,17 @@ class ClientsFragment : Fragment() {
     }
 
     private fun setupToolbar() {
-
-//        binding.toolbar.inflateMenu(R.menu.clients_menu)
-//        binding.toolbar.setOnMenuItemClickListener { item ->
-//            when (item.itemId) {
-//                R.id.action_add_client -> {
-//                    findNavController().navigate(R.id.action_clientsFragment_to_clientFragmentAdd)
-//                    true
-//                }
-//                else -> false
-//            }
-//        }
+        binding.toolbar.title = "Clients"
+        binding.toolbar.inflateMenu(R.menu.clients_menu)
+        binding.toolbar.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.action_add_client -> {
+                    findNavController().navigate(R.id.action_clientsFragment_to_clientFragmentAdd)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     private fun setupView() {
